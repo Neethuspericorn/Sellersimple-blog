3b13ea5d52a509dd5ad3933b83401d55c7c40865
Anooperajendran <anoop.rajendran@spericorn.com>
2023-02-21 16:47:07 +0530

    development blog

---
+++ js/footer.js
@@ -0,0 +1,107 @@
+var footer = ` <div class="footer-section">
+<div class="sp-container container-custom sp-d-flex">
+    <div class="footer-top-wrapper wid-out f-logo-block">
+        <div class="footer-col-block footer-otr">
+            <div class="footer-details-cover">
+                <p class="footer-logo m-0"><a href="https://www.spericorn.com/"><img src="../img/logo-dark.svg" alt="development" width="162" height="38"></a></p>
+                <div class="contact-block"><a class="contact-link" href="mailto:contact@spericorn.com"><span class="icon"><img src="../img/mail-sm.svg" alt="Mail" width="21" height="14"></span><span class="icon-label">contact@spericorn.com</span></a><a class="contact-link"
+                        href="tel:+91 471-4612777"><span class="icon"><img src="../img/phone-sm.svg" alt="Phone" width="21" height="14"></span><span class="icon-label">+91 471-4612777</span></a><a class="contact-link" href="skype:service.spericorn?call"><span class="icon"><img src="../img/serv-sm.svg" alt="Service" width="21" height="14"></span><span class="icon-label">service.spericorn</span></a></div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-top-wrapper flex-1">
+        <div class="footer-col-block">
+            <div class="link-outer">
+                <div class="ft-hed">Quick Links</div>
+                <ul class="footer-ul">
+                    <li><a href="https://www.spericon.in/" target="_blank">Home</a></li>
+                    <li><a href="../about-us.html" target="_blank">About</a></li>
+                    <li><a href="../case-studies.html" target="_blank">Case Studies</a></li>
+                </ul>
+                <ul class="footer-ul">
+
+                    <li><a href="../careers.html" target="_blank">Careers</a></li>
+                    <li><a href="../blogs.html" target="_blank">Blogs</a></li>
+                    <li><a href="../interview-status.html" target="_blank">Check my interview status </a></li>
+                    <li><a href="../contact-us.html" target="_blank">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-col-block">
+            <div class="link-outer">
+                <div class="ft-hed">Technologies</div>
+                <ul class="footer-ul">
+                    <li><a href="../angular.html">Angular</a></li>
+                    <li><a href="../react-js.html">React JS</a></li>
+                    <li><a href="../node-js.html">Node JS</a></li>
+                </ul>
+                <ul class="footer-ul">
+                    <li><a href="../python.html">Python</a></li>
+                    <li><a href="../javascript.html">Javascript</a></li>
+                    <li><a href="../wordpress.html">Wordpress</a></li>
+                    <li><a href="../php.html">PHP</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-col-block">
+            <div class="link-outer">
+                <div class="ft-hed">Expertise</div>
+                <ul class="footer-ul">
+                    <li><a href="../laravel.html">Laravel</a></li>
+                    <li><a href="../codeigniter.html">CodeIgniter</a></li>
+                    <li><a href="../coldfusion.html">ColdFusion</a></li>
+                </ul>
+                <ul class="footer-ul">
+                    <li><a href="../dot-net.html">Dot Net</a></li>
+                    <li><a href="../android.html">Android</a></li>
+                    <li><a href="../react-native.html">React Native</a></li>
+                    <li><a href="../ionic.html">Ionic</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-col-block">
+            <div class="link-outer">
+                <div class="ft-hed">Services</div>
+                <ul class="footer-ul">
+                    <li><a href="../web-app-development.html" target="_blank">Web App Development</a></li>
+                    <li><a href="../mob-app.html" target="_blank">Mobile App Development</a></li>
+                    <li><a href="../digital-marketing.html" target="_blank">Digital Marketing</a></li>
+                </ul>
+                <ul class="footer-ul">
+                    <li><a href="../iot-development.html" target="_blank">IOT Development</a></li>
+                    <li><a href="../web-design.html" target="_blank">Web Designing</a></li>
+                    <li><a href="../blockchain-development.html" target="_blank">Blockchain Development</a></li>
+                    <li><a href="../artificial-intelligence.html" target="_blank">Artificial Intelligence Solutions</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<div class="copy-right-section">
+<div class="sp-container container-custom">
+    <div class="copy-right-cover">
+        <div class="follow-us-section">
+            <div class="social-icon-outer sp-d-flex"><a href="https://www.facebook.com/spericorntechnology/" target="_blank"><img src="../img/facebook.svg" alt="facebook" width="11" height="24"></a><a href="https://twitter.com/spericorn" target="_blank"><img src="../img/twitter.svg" alt="twitter" width="22" height="18"></a>
+                <a
+                    href="https://www.linkedin.com/company/spericorn-technology-pvt-ltd?originalSubdomain=in" target="_blank"><img src="../img/linkedin.svg" alt="linkedin" width="25" height="18"></a><a href="https://www.instagram.com/spericorntechnology/" target="_blank"><img src="../img/instagram.svg" alt="linkedin" width="30" height="16"></a></div>
+        </div>
+        <div class="copy-text">
+            <p><span id="copyright-year"></span> Spericorn Technology Inc. All Rights Reserved |<a href="../privacy-policy.html">Privacy Policy</a></p>
+        </div>
+        <div class="app-icon-outer sp-d-flex">
+            <a href="https://apps.apple.com/us/app/erp-spericorn/id6444275248" target="_blank">
+                <img src="../img/apple_footer.png" alt="Ios App Downlaod" width="100" height="25">
+            </a>
+            <a href="https://play.google.com/store/apps/details?id=com.spericorn.erp&pli=1" target="_blank">
+                <img src="../img/google_footer.png" alt="Ios App Downlaod" width="100" height="25">
+            </a>
+
+        </div>
+    </div>
+</div>
+</div>
+`;
+const footr = document.getElementById("footer-section");
+
+footr.insertAdjacentHTML("beforeend", footer);
